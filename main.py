9236a15b3d132bfc862112b58edb3a4792d74f58
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-06 22:19:54 +0500

    Implement account creation and data persistence
    Refactored Bank class to include JSON data handling and account creation logic.

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
+import json
+import random
+import string
+from pathlib import Path
+
 
 class Bank:
-    def createaccount(self):
-        pass
+    database = 'data.json'
+    data = []
 
-    
+    try:
+        if Path(database).exists():
+            with open(database) as fs:
+                data = json.loads(fs.read())
+        else:
+            print("No such file exists")
+    except Exception as err:
+        print(f"An exception occurs as {err}")
+
+    @staticmethod
+    def update():
+        with open(Bank.database, 'w') as fs:
+            fs.write(json.dumps(Bank.data))
+
+    def createaccount(self):
+        data = {
+            "name": input("What is your Name: "),
+            "age": int(input("What is your Age: ")),
+            "email": input("Write email: "),
+            "pin": int(input("Write your 4 numbers pin code: ")),
+            "accountNo.": 1234,
+            "balance": 0
+        }
+        if data['age'] < 18 or len(str(data['pin'])) != 4:
+            print("Sorry you cannot create your account")
+        else:
+            print("Account has been created successfully")
+            for i in data:
+                print(f"{i} : {data[i]}")
+            print("note down your account number")
+
+            Bank.data.append()
+
+            Bank.update(data)
+
+
 user = Bank()
 
 print("Press 1 for Creating an Account")
